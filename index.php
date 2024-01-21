c6084d893e778762ff8696d14fd28971ba304f62
VikramGowdru <143245936+VikramGowdru@users.noreply.github.com>
2024-01-21 23:42:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -19,3 +19,27 @@ try {
 $pdo = null;
 
 ?>
+
+########################################################################################################################
+<!DOCTYPE html>
+<html>
+<head>
+  <title>MySQL Connection Test</title>
+</head>
+<body>
+
+<?php
+$conn = new mysqli('my-mysql-instance.c7oaea88gfev.ap-south-1.rds.amazonaws.com:3306', 'admin', 'vikram123', 'mydatabase');
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "<h2>Connected to MySQL successfully</h2>";
+
+$conn->close();
+?>
+
+</body>
+</html>
